428938ff27ade4deed2a12f42166ec25da17cfa6
Khanh Tung <tung_it@vfr.net.vn>
2025-09-22 11:21:44 +0700

    structure app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/data/repository/repository.dart';
+import 'package:music_app/ui/home/home.dart';
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  var repository = DefaultRepository();
-  var songs = await repository.fetchSongs();
-  for(var song in songs!){
-    debugPrint(song.toString());
-  }
-  // runApp(const MainApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // var repository = DefaultRepository();
+  // var songs = await repository.fetchSongs();
+  // for(var song in songs!){
+  //   debugPrint(song.toString());
+  // }
+  runApp(const MusicApp());
 }
 
 class MainApp extends StatelessWidget {
